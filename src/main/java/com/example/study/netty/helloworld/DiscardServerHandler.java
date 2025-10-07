1bf73146c370a29eccc2c2f48038dcd141dc443b
thelovemsg <thelovemsg@naver.com>
2025-10-07 16:47:06 +0900

    주석 추가

--- src/main/java/com/example/study/netty/helloworld/DiscardServerHandler.java
+++ src/main/java/com/example/study/netty/helloworld/DiscardServerHandler.java
@@ -1,21 +1,24 @@
 package com.example.study.netty.helloworld;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.Channel;
+
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.SimpleChannelInboundHandler;
 
-public class DiscardServerHandler extends SimpleChannelInboundHandler<Object> {
+/**
+ * Handles a server-side channel.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
 
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
-//        Channel
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // Discard the received data silently.
+        ((ByteBuf) msg).release(); // (3)
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
         cause.printStackTrace();
         ctx.close();
     }
